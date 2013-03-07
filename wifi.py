9f79771e15258b63935317b1aa812790d3936c5f
Darren Maczka <dmaczka@vt.edu>
2013-03-07 14:51:32 -0500

    add color/icon change to wifi depending on state

--- wifi.py
+++ wifi.py
@@ -12,13 +12,32 @@ class WifiWidget(object):
             out = out.decode('utf-8')
             m = re.match(r'{interface}\s+ESSID:"(.+?)"\n'.format(
                 interface=self.interface), out)
-
+            
+            color = '#ff8888'
+            out = subprocess.check_output(['wpa_cli','status'])
+            out = out.decode('utf-8')
+            ip = 'none'
+            for line in out.split('\n'):
+                #print("checking: {0}".format(line))
+                ipm = re.match(r'ip_address=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)', line)
+                if ipm:
+                    ip = ipm.group(1)
+                    color = '#7adf8f'
+                    break
+            
             return {
                 'name': "wifi",
                 'instance': self.interface,
                 'full_text': ' ' + m.group(1),
-                'color': '#dfaf8f',
-                'icon': 'mmbar/icons/wifi_02.xbm',
+                'color': color,
+                'icon': 'mmbar/icons/wifi_03.xbm',
             }
         except subprocess.CalledProcessError:
+            return {
+                'name': "wifi",
+                'instance': self.interface,
+                'full_text': ' ',
+                'color': '#3f3f3f',
+                'icon': 'mmbar/icons/wifi_off_03.xbm',
+            }
             pass
